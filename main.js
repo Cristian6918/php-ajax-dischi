bca90a955df1464ee89dca444a5a712fdd5166ea
Cristian Tulbu <96737712+Cristian6918@users.noreply.github.com>
2022-05-05 22:56:07 +0200

    done

--- main.js
+++ main.js
@@ -1,22 +1,60 @@
 console.log("js ok");
 
+
 const app = new Vue({
     el: "#app",
     data() {
         return {
             albums: [],
+            albumGenres: [],
+            selected: "All",
         }
     },
     mounted() {
-        axios.get("http://localhost/php-ajax-dischi/data.php").then((result) => {
-            if(result.status===200){
-                console.log(result.data)
-                this.albums = result.data;
+        this.loadData();
+        
+       
+       
+    },
+    methods: {
+
+        
+        loadData() {
+            axios.get("http://localhost/php-ajax-dischi/data.php").then((result) => {
+                if (result.status === 200) {
+                    this.albums = result.data;
+                    this.albums.forEach(element => {
+                        if (!this.albumGenres.includes(element.genre)) {
+                            this.albumGenres.push(element.genre);
+                        }
+                    })
+                
                 }
             
-        }).catch((err) => {
-            console.log(err)
+            }).catch((err) => {
+                console.log(err)
             
-        });
+            });
+            
+        },
+        onChange(event) {
+            this.selected=event.target.value;
+            
+        }
+
+        
+
+    },
+    computed: {
+        filteredList() {
+            if (this.selected === "All") {
+                return this.albums;
+            } else {
+                return this.albums.filter((item) => item.genre===this.selected)
+            }
+
+      }
     }
-})
+   
+});
+
